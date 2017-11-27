c01df78e3b4fe22615bced5bf909dcf77c4c7728
Felipe da Silva <felipe.silva@corebiz.com.br>
2017-11-27 15:54:28 -0200

    fix: adjust produto.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var app = require('./config/express')();
 var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000, function(){
-	console.log('Servidor rodando');
+	console.log('Servidor rodando...');
 });
